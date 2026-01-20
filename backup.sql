26660000090c586f58f2108c7d7a4f94d302ce21
Richard Sebold <richardsebold21@gmail.com>
2026-01-19 22:36:53 -0300

    first commit

---
+++ backup.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict jFLL02lZgXIhJIB2aJmcd5kg8AiiKM27d1yajOMv4ImIGcmYr71byGczn5mlsnU
+
+-- Dumped from database version 18.1
+-- Dumped by pg_dump version 18.1
+
+-- Started on 2026-01-19 22:33:25
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-01-19 22:33:26
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict jFLL02lZgXIhJIB2aJmcd5kg8AiiKM27d1yajOMv4ImIGcmYr71byGczn5mlsnU
+
